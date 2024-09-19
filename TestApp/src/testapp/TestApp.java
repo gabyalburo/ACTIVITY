f14a0db303460244cb6973f6e60754a35acc0555
Coronado, Rein Justin H. <reincoronado14@gmail.com>
2024-09-19 09:48:49 +0800

    Add files via upload

---
+++ TestApp/src/testapp/TestApp.java
@@ -0,0 +1,20 @@
+package testapp;
+
+public class TestApp {
+    
+    public static void main(String[] args) {
+        
+        //Implementor = holds
+        
+        //Execution = user input
+        
+        Salary salary = new Salary();
+        
+        salary.menu();
+        
+        
+        
+        
+    }
+    
+}
